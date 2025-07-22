0e64ce6804e20019844654d838e6d1e8f6e8f16a
clin2on3mun <munanaclinton@gmail.com>
2025-07-22 15:53:40 +0200

    Add changes to layout

--- frontend/src/pages/Layout.tsx
+++ frontend/src/pages/Layout.tsx
@@ -1,22 +1,19 @@
 import { Outlet } from "react-router-dom";
 import TopNav from "../components/TopNav";
 import { useAuth } from "../hooks/AuthHooks";
-import { useNavigate } from "react-router-dom";
 import { ClipLoader } from "react-spinners";
-import { useEffect} from "react";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+
 
 export default function Layout() {
   const {user,isLoading} = useAuth();
- 
-
-  const navigate = useNavigate()
-  
-   useEffect(()=>{
+  const navigate =  useNavigate()
+  useEffect(()=>{
     if(!user && !isLoading){
     navigate('/login')
   }
-  }, [user])
-
+  },[user, isLoading])
   if (isLoading) {
     return (
       <div className="flex justify-center min-h-screen items-center">
@@ -25,10 +22,9 @@ export default function Layout() {
     );
   }
   
-
   return (
     <>
-      <main className="max-w-7xl px-4 mx-auto">
+    <main className="max-w-7xl  px-4 mx-auto">
         <TopNav />
         <Outlet />
       </main>
